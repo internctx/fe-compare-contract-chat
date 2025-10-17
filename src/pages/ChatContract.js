1ae02edac9aac36f1734f7080104dfd541dabd41
internctx <repo.internctx@gmail.com>
2025-10-18 03:08:07 +0700

    Update ChatContract.js

--- src/pages/ChatContract.js
+++ src/pages/ChatContract.js
@@ -29,7 +29,7 @@ const ChatContract = () => {
   const [dropdown, setDropDown] = useState(false);
   const [documentID,setDocumentID] = useState("")
   const [documentName,setDocumentName] = useState("")
-  const urlProcess = "https://compare-contract-dev-duaggrapf3cnhnbk.japaneast-01.azurewebsites.net"
+  const urlProcess = "https://compare-contract-dzhqg5h3bcfdbtab.southeastasia-01.azurewebsites.net"
   // const urlProcess = "http://127.0.0.1:8003"
   const EXPIRATIONS_EXTRA = 24;
 
