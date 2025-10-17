cd5f74a4b77176735e2bb489f608478c6fc35064
internctx <repo.internctx@gmail.com>
2025-10-18 03:09:22 +0700

    Update CompareContractNew.js

--- src/pages/CompareContractNew.js
+++ src/pages/CompareContractNew.js
@@ -5,7 +5,7 @@ import ResponseClause from "../compoments/ResponseClause";
 import { BsChatRightFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 const CompareContractNew = () => {
-  const url_compare = "https://compare-contract-dev-duaggrapf3cnhnbk.japaneast-01.azurewebsites.net"
+  const url_compare = "https://compare-contract-dzhqg5h3bcfdbtab.southeastasia-01.azurewebsites.net"
   // const url_compare = "http://127.0.0.1:8003";
   const navigate = useNavigate();
   const [content,setContent] = useState(null)
